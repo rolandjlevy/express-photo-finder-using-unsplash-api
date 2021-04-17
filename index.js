1dabb0f4ee3a79c59ec7147d2808ded862a8c691
Roland Levy <contact@rolandlevy.co.uk>
2021-04-17 18:19:33 +0000

    Refactored

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3000;
 const ACCESSKEY = process.env.ACCESSKEY;
 const BASEURL = process.env.BASEURL;
 const PERPAGE = 30;
-let uri = '/';
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,48 +15,66 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
 app.use((req, res, next) =>  {
-  uri = req.originalUrl;
+  res.locals.uri = req.originalUrl;
   next();
 })
 
 app.post('/search', (req, res) => {
   const { searchquery } = req.body;
   const page = 1;
-  fetchImages(searchquery, page).then(output => {
-    res.render('pages/search', { output, uri });
-  });
+  try {
+    fetchImages(searchquery, page).then(output => {
+      res.render('pages/search', { 
+        output, 
+        uri:res.locals.uri 
+      })
+    });
+  } catch(err) {
+    next(err);
+  }
 });
 
 app.get('/search', (req, res) => {
   const { query, page, totalpages } = req.query;
   let p = Number(page) || 1;
-  p = p > totalpages ? totalpages : p;
+  if (p > Number(totalpages)) p = Number(totalpages);
   fetchImages(query, p).then(output => {
-    res.render('pages/search', { output, uri });
+    res.render('pages/search', { 
+      output, 
+      uri:res.locals.uri
+    });
   });
 });
 
 app.get('/', (req, res) => {
-  res.render('pages/index', { uri });
+  res.render('pages/index', { uri:res.locals.uri });
 });
 
 app.get('/about', (req, res) => {
-  res.render('pages/about', { uri });
+  res.render('pages/about', { uri:res.locals.uri });
 });
 
 app.get('/favourites', (req, res) => {
   const { page, query } = req.query;
-  res.render('pages/favourites', { page, query, uri } );
-});
-
-app.listen(PORT, () => {
-  console.log('Listening on port', PORT);
+  res.render('pages/favourites', { 
+    page, 
+    query, 
+    uri:res.locals.uri
+  });
 });
 
 function fetchImages(query, page) {
+  const url = `${BASEURL}?page=${page}&query=${query}&per_page=${PERPAGE}&client_id=${ACCESSKEY}`;
+  let error;
   return new Promise((resolve, reject) => {
-    fetch(`${BASEURL}?page=${page}&query=${query}&per_page=${PERPAGE}&client_id=${ACCESSKEY}`)
-    .then(result => result.json())
+    fetch(url)
+    .then(result => {
+      if (!result.ok) {
+        const error = new Error(`Unable to load: ${url}`);
+        reject(error);
+      }
+      return result.json();
+    })
     .then(data => {
       const { results, total, total_pages } = data;
       const output = {
@@ -68,6 +85,39 @@ function fetchImages(query, page) {
         results
       };
       resolve(output);
-    });
+    })
   });
 }
+
+app.listen(PORT, () => {
+  console.log('Listening on port', PORT);
+});
+
+  /*////////////////*/
+  /* Error handling */
+  /*////////////////*/
+
+// page not found (404)
+app.get('/not-found', (req, res) => {
+  res.status(404);
+  res.render('pages/not-found');
+});
+
+// wildcard route throws 302 error (temporary redirect)
+app.get('*', (req, res, next) => {
+  const error = new Error(`${req.ip} tried to access ${req.originalUrl}`);
+  error.statusCode = 302;
+  next(error);
+});
+// middleware for handing errors
+app.use((error, req, res, next) => {
+  // if status code not defined set to generic HTTP status code (500)
+  if (!error.statusCode) error.statusCode = 500;
+  // redirect if route is not found
+  if (error.statusCode === 302) {
+    return res.status(302).redirect('/not-found');
+  }
+  // render error page
+  res.status(error.statusCode);
+  res.render('pages/error', { error:error.toString() })
+});
