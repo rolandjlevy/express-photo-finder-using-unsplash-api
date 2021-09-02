fc0552fc27b195bc4044ee6c1a2c1418482358ec
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-09-02 20:20:01 +0000

    Added random-words node package

--- utils.js
+++ utils.js
@@ -1,6 +1,12 @@
+const randomWords = require('random-words');
+
 const randomNum = (n) => Math.round(Math.random() * n) - 1;
 
-const words = ['welcome', 'hello world', 'chocolate', 'ice cream', 'sunshine', 'green forest', 'clear blue sky', 'calm sea', 'peaceful sunrise', 'summer breeze', 'relaxing beach', 'swimming pool', 'blue lagoon', 'clouds'];
+const wordsFromPackage = randomWords({exactly: 25, maxLength: 8});
+
+const wordsFromLocal = ['welcome', 'hello world', 'chocolate', 'ice cream', 'sunshine', 'green forest', 'clear blue sky', 'calm sea', 'peaceful sunrise', 'summer breeze', 'relaxing beach', 'swimming pool', 'blue lagoon', 'clouds'];
+
+const words = wordsFromPackage.length ? wordsFromPackage : wordsFromLocal;
 
 const getRandomWord = () => {
   const num = randomNum(words.length);
