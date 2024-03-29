a76a1f2264c5fc10ee89d609fab0783240b043b0
Roland Levy <contact@rolandlevy.co.uk>
2024-03-29 20:53:25 +0000

    collapse navbar when user clicks outside navbar area

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const localResults = window.localStorage.getItem('results');
 const results = JSON.parse(localResults);
 const localSaved = window.localStorage.getItem('saved') || '[]';
-let saved = JSON.parse(localSaved).filter(item => !!item);
+let saved = JSON.parse(localSaved).filter((item) => !!item);
 const savedFavourites = document.querySelector('.saved-favourites');
 savedFavourites.innerHTML = `Favourites (${saved.length})`;
 let favourites;
@@ -11,17 +11,19 @@ function generateFavourites(target = 'results') {
   const arr = target === 'results' ? results : saved;
   // console.log({arr});
   favourites = document.querySelectorAll('.image > .favourite');
-  favourites.forEach(img => {
-    const isSaved = saved.find(item => item.id === img.id);
-    if (isSaved) img.classList.add('on');
+  favourites.forEach((img) => {
+    const isSaved = saved.find((item) => item.id === img.id);
+    if (isSaved) {
+      img.classList.add('on');
+    }
     img.addEventListener('click', (e) => {
       const classList = e.target.classList;
-      const foundInSaved = saved.find(item => item.id === e.target.id);
+      const foundInSaved = saved.find((item) => item.id === e.target.id);
       if (foundInSaved) {
-        saved = saved.filter(item => item.id !== e.target.id);
+        saved = saved.filter((item) => item.id !== e.target.id);
         classList.remove('on');
       } else {
-        const foundObj = arr.find(item => item.id === e.target.id);
+        const foundObj = arr.find((item) => item.id === e.target.id);
         saved.push(foundObj);
         classList.add('on');
       }
@@ -31,3 +33,16 @@ function generateFavourites(target = 'results') {
     });
   });
 }
+
+// collapse navbar when user clicks outside navbar area
+document.body.addEventListener('click', function (event) {
+  const navbar = document.querySelector('.navbar-collapse');
+  const navbarToggler = document.querySelector('.navbar-toggler');
+  if (
+    navbar.classList.contains('show') &&
+    !navbar.contains(event.target) &&
+    event.target !== navbarToggler
+  ) {
+    navbar.classList.remove('show');
+  }
+});
